6b8aca6f4a248f8062dda079d0978fc414583583
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-26 11:09:13 +0200

    Add tests for DefaultGenerateFaces.

---
+++ src/test/java/net/imagej/ops/descriptor3d/DefaultGenerateFacesTest.java
@@ -0,0 +1,98 @@
+package net.imagej.ops.descriptor3d;
+import static org.junit.Assert.assertTrue;
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.Cursor;
+import net.imglib2.FinalDimensions;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.view.Views;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultGenerateFacesTest extends AbstractOpTest {
+
+	private Img<BitType> img;
+
+	@Before
+	public void init() {
+		img = new ArrayImgFactory<BitType>().create(new FinalDimensions(9, 9, 9), new BitType());		
+	}
+	
+	@Test
+	public void gnerateTestPoint() {
+		resetImg();
+		Cursor<BitType> c = Views.interval(img, new long[]{0, 0, 0}, new long[]{1, 1, 1}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		
+		DefaultFaces faces = (DefaultFaces)ops.run(GenerateFaces.class, new DefaultFaces(), img, 1);
+
+		assertTrue(faces.getArea() == 6);
+	}
+	
+	@Test
+	public void gnerateTest1x1() {
+		resetImg();
+		Cursor<BitType> c = Views.interval(img, new long[]{0, 0, 0}, new long[]{1, 1, 1}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		
+		DefaultFaces faces = (DefaultFaces)ops.run(GenerateFaces.class, new DefaultFaces(), img, 1);
+
+		assertTrue(faces.getArea() == 6);
+	}
+	
+	@Test
+	public void gnerateTest2x2() {
+		resetImg();
+		Cursor<BitType> c = Views.interval(img, new long[]{1, 1, 1}, new long[]{3, 3, 3}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		
+		DefaultFaces faces = (DefaultFaces)ops.run(GenerateFaces.class, new DefaultFaces(), img, 1);
+
+		assertTrue(faces.getArea() == 24);
+	}
+	
+	@Test
+	public void gnerateTest2x2GridSize() {
+		resetImg();
+		Cursor<BitType> c = Views.interval(img, new long[]{2, 2, 2}, new long[]{7, 7, 7}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		
+		DefaultFaces faces = (DefaultFaces)ops.run(GenerateFaces.class, new DefaultFaces(), img, 1);
+		
+		assertTrue(faces.getArea() == 150);
+	}
+	
+	@Test
+	public void gnerateTest1x1And1x1() {
+		resetImg();
+		Cursor<BitType> c = Views.interval(img, new long[]{1, 1, 1}, new long[]{2, 2, 2}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		c = Views.interval(img, new long[]{3, 3, 3}, new long[]{4, 4, 4}).cursor();
+		while (c.hasNext()) {
+			c.next().setOne();
+		}
+		
+		DefaultFaces faces = (DefaultFaces)ops.run(GenerateFaces.class, new DefaultFaces(), img, 1);
+		
+		assertTrue(faces.getArea() == 12);
+	}
+	
+	private void resetImg() {
+		Cursor<BitType> c = img.cursor();
+		while (c.hasNext()) {
+			c.next().setZero();
+		}
+	}
+}
