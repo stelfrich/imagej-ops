7bd931caaea4fcc215382734470ce319cfd7d4fd
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-08 21:51:46 +0200

    Add hash and equals.

--- src/main/java/net/imagej/ops/descriptor3d/Vector3d.java
+++ src/main/java/net/imagej/ops/descriptor3d/Vector3d.java
@@ -4,12 +4,51 @@ import net.imglib2.RealPoint;
 
 public class Vector3d extends RealPoint {
 
+	private double x;
+	private double y;
+	private double z;
+
 	public Vector3d(final double x, final double y, final double z) {
 		super(x,y,z);
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 
 	public double getX() {
 		return getDoublePosition(0);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector3d other = (Vector3d) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		if (Double.doubleToLongBits(z) != Double.doubleToLongBits(other.z))
+			return false;
+		return true;
 	}
 
 	public double getY() {
@@ -35,4 +74,5 @@ public class Vector3d extends RealPoint {
 	public String toString() {
 		return "X: " + this.getX() + ", Y: " + this.getY() + ", Z: " + this.getZ();
 	}
+	
 }
