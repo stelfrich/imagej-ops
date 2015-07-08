6d1d52cd66edb753e000f54fa1ba12afa9b35b8c
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-08 21:52:21 +0200

    Add centroid and surface points.

--- src/main/java/net/imagej/ops/descriptor3d/DefaultFaces.java
+++ src/main/java/net/imagej/ops/descriptor3d/DefaultFaces.java
@@ -1,16 +1,24 @@
 package net.imagej.ops.descriptor3d;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class DefaultFaces implements Faces, Iterable<DefaultFace> {
 
 	private ArrayList<DefaultFace> faces;
 	
+	private HashSet<Vector3d> points;
+	
 	private double area;
+	
+	private Vector3d centroid;
 
 	public DefaultFaces() {
 		faces = new ArrayList<DefaultFace>();
+		points = new HashSet<Vector3d>();
 		area = 0;
 	}
 
@@ -25,10 +33,35 @@ public class DefaultFaces implements Faces, Iterable<DefaultFace> {
 	public void addFace(DefaultFace f) {
 		faces.add(f);
 		area += f.getArea();
+
+		points.addAll(Arrays.asList(f.getVertices()));
+	}
+	
+	public HashSet<Vector3d> getPoints() {
+		return points;
 	}
 
 	public double getArea() {
 		return area;
+	}
+	
+	public Vector3d getCentroid() {
+		if (centroid == null) {
+			Iterator<Vector3d> it = points.iterator();
+			double x,y,z = y = x = 0;
+			while (it.hasNext()) {
+				Vector3d next = it.next();
+				x += next.getX();
+				y += next.getY();
+				z += next.getZ();
+			}
+			
+			x /= points.size();
+			y /= points.size();
+			z /= points.size();
+			centroid = new Vector3d(x, y, z);
+		}
+		return centroid;
 	}
 
 	@Override
