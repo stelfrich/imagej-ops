704d441a85f862eb2253b34c8c83d47f5a8eae46
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-08 21:53:02 +0200

    Remove Contingent.

--- src/main/java/net/imagej/ops/descriptor3d/GenerateFaces.java
+++ src/main/java/net/imagej/ops/descriptor3d/GenerateFaces.java
@@ -1,11 +1,10 @@
 package net.imagej.ops.descriptor3d;
 
-import net.imagej.ops.Contingent;
 import net.imagej.ops.OutputFunction;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.Type;
 
 public interface GenerateFaces<I extends Type<I>, O extends Faces> extends
-		OutputFunction<RandomAccessibleInterval<I>, O>, Contingent {
+		OutputFunction<RandomAccessibleInterval<I>, O> {
 	// NB: Marker Interface
 }
