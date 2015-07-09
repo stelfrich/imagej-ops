39e4c7af538dde5674a18a7f7dcd3473624e0d2b
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-09 18:47:29 +0200

    Normalize moments.

--- src/main/java/net/imagej/ops/descriptor3d/MultiVariate3D.java
+++ src/main/java/net/imagej/ops/descriptor3d/MultiVariate3D.java
@@ -2,21 +2,32 @@ package net.imagej.ops.descriptor3d;
 
 import net.imagej.ops.AbstractOutputFunction;
 import net.imagej.ops.Contingent;
+import net.imagej.ops.Op;
+import net.imagej.ops.features.geometric.Geometric3DFeatures.VolumeFeature;
 import net.imglib2.Cursor;
-import net.imglib2.IterableInterval;
-import net.imglib2.type.numeric.RealType;
+import net.imglib2.roi.IterableRegion;
+import net.imglib2.type.BooleanType;
+import net.imglib2.type.numeric.real.DoubleType;
 
-public class Variance3D<T extends RealType<T>> extends
-		AbstractOutputFunction<IterableInterval<T>, Moments2> implements Contingent {
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
 
+@Plugin(type = Op.class, name = "multivariate3d")
+public class MultiVariate3D<B extends BooleanType<B>> extends
+		AbstractOutputFunction<IterableRegion<B>, Covariance3D> implements Contingent {
+
+	@Parameter(type = ItemIO.INPUT)
+	private VolumeFeature<DoubleType> volume;
+	
 	@Override
-	public Moments2 createOutput(IterableInterval<T> input) {
-		return new Moments2(0, 0, 0, 0, 0, 0);
+	public Covariance3D createOutput(IterableRegion<B> input) {
+		return new Covariance3D();
 	}
 
 	@Override
-	protected Moments2 safeCompute(IterableInterval<T> input, Moments2 output) {
-		Cursor<T> c = input.localizingCursor();
+	protected Covariance3D safeCompute(IterableRegion<B> input, Covariance3D output) {
+		Cursor<B> c = input.localizingCursor();
 		int[] pos = new int[3];
 		double mX = (input.max(0) + input.min(0)) / 2;
 		double mY = (input.max(1) + input.min(1)) / 2;
@@ -24,19 +35,27 @@ public class Variance3D<T extends RealType<T>> extends
 		while (c.hasNext()) {
 			c.next();
 			c.localize(pos);
-			output.setS200(output.getS00() + (pos[0] - mX) * (pos[0] - mX));
-			output.setS110(output.getS01() + (pos[0] - mX) * (pos[1] - mY));
-			output.setS101(output.getS02() + (pos[0] - mX) * (pos[2] - mZ));
-			output.setS020(output.getS11() + (pos[1] - mY) * (pos[1] - mY));
-			output.setS011(output.getS12() + (pos[1] - mY) * (pos[2] - mZ));
-			output.setS002(output.getS22() + (pos[2] - mZ) * (pos[2] - mZ));
+			output.setS200(output.getS200() + (pos[0] - mX) * (pos[0] - mX));
+			output.setS020(output.getS020() + (pos[1] - mX) * (pos[1] - mY));
+			output.setS002(output.getS002() + (pos[2] - mX) * (pos[2] - mZ));
+			output.setS110(output.getS110() + (pos[0] - mY) * (pos[1] - mY));
+			output.setS101(output.getS101() + (pos[0] - mY) * (pos[2] - mZ));
+			output.setS011(output.getS011() + (pos[1] - mZ) * (pos[2] - mZ));
 		}
+		
+		output.setS200(output.getS200() / volume.getOutput().get());
+		output.setS020(output.getS020() / volume.getOutput().get());
+		output.setS002(output.getS002() / volume.getOutput().get());
+		output.setS110(output.getS110() / volume.getOutput().get());
+		output.setS101(output.getS101() / volume.getOutput().get());
+		output.setS011(output.getS011() / volume.getOutput().get());
+		
 		return output;
 	}
 
 	@Override
 	public boolean conforms() {
-		return getInput().numDimensions() == 3;
+		return true;
 	}
 
 }
