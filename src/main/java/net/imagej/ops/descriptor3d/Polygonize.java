f0d6f788b583094688c296190ec18adcf7731b40
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-10 17:55:30 +0200

    Rename GenerateFaces to Poligonize.

--- src/main/java/net/imagej/ops/descriptor3d/Polygonize.java
+++ src/main/java/net/imagej/ops/descriptor3d/Polygonize.java
@@ -4,7 +4,7 @@ import net.imagej.ops.OutputFunction;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.Type;
 
-public interface GenerateFaces<I extends Type<I>, O extends Faces> extends
+public interface Polygonize<I extends Type<I>, O extends Faces> extends
 		OutputFunction<RandomAccessibleInterval<I>, O> {
 	// NB: Marker Interface
 }
