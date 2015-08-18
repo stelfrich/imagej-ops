8138dfcba6387b10bb020680a791d81fe1713c0b
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-08-18 16:48:03 +0200

    Clean up because of Face to Facet rename.

--- src/main/java/net/imagej/ops/descriptor3d/Polygonize.java
+++ src/main/java/net/imagej/ops/descriptor3d/Polygonize.java
@@ -4,7 +4,7 @@ import net.imagej.ops.OutputFunction;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.Type;
 
-public interface Polygonize<I extends Type<I>, O extends Faces> extends
+public interface Polygonize<I extends Type<I>, O extends Facets> extends
 		OutputFunction<RandomAccessibleInterval<I>, O> {
 	// NB: Marker Interface
 }
